f72b075c7f7ddb0a2700b64f8e1d051bd2ca9b35
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-06-11 21:03:06 +0530

    First Commit

---
+++ hello_world.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!" + " Welcome to GO World")
+	if 5 > 1 {
+		fmt.Println("Then it is a bigger number!")
+	}
+	fmt.Println("Let's Go and Run the Code")
+	fmt.Println("This is an another statement in the flow")
+}
